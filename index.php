6629107efe9626432729af881af2679105517286
Tyler J. Smith <smithtj921@gmail.com>
2021-02-01 20:11:32 -0500

    Github first commit

--- index.php
+++ index.php
@@ -1,2 +1,25 @@
 <?php
+
+//Get Heroku ClearDB connection information
+// $cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
+// $cleardb_server   = $cleardb_url["host"];
+// $cleardb_username = $cleardb_url["user"];
+// $cleardb_password = $cleardb_url["pass"];
+// $cleardb_db       = substr($cleardb_url["path"], 1);
+
+$active_group = 'default';
+$query_builder = TRUE;
+
+// Connect to DB
+//$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+$mysqli = new mysqli('us-cdbr-east-03.cleardb.com', 'b27268e1e174f3', 'a5769c7d', 'heroku_ea94c1083a34040');
+
 echo "Hello buggy project!!";
+echo "hello";
+
+$result = $mysqli->query("SELECT * FROM userinfo");
+if ($result->num_rows) {
+    $row = $result->fetch_array();
+    echo print_r($row);
+}
