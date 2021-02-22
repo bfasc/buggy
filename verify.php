5872ce690a981e0a7d8e2c1070461541dae3bb2b
Tyler J. Smith <smithtj921@gmail.com>
2021-02-22 00:20:08 -0500

    vs code workings

--- verify.php
+++ verify.php
@@ -1,36 +1,38 @@
 <?php
-    REQUIRE_ONCE 'assets/functions.php';
-    REQUIRE_ONCE 'php-files/verify.php';
-    printHead("Verify Your Account | Buggy - Let's Code Together");
+require_once 'assets/functions.php';
+require_once 'php-files/verify.php';
+printHead("Verify Your Account | Buggy - Let's Code Together");
 
-    $response = "";
-    if(isset($_GET['code']) && !empty($_GET['code']) && isset($_GET['email']) && !empty($_GET['email'])) {
-        if(checkCode($_GET['code'], $_GET['email'])) {
-            if(!checkVerified($_GET['email'])) {
+$response = "";
+if (isset($_GET['code']) && !empty($_GET['code']) && isset($_GET['email']) && !empty($_GET['email'])) {
+    if (checkCode($_GET['code'], $_GET['email'])) {
+        if (!checkVerified($_GET['email'])) {
 
-                verifyAccount($_GET['email']);
+            verifyAccount($_GET['email']);
 
-                //check if management account needs purchase
-                $userID = getUserID($_GET['email']);
-                if(getAccountType($userID) == "management") { //management account
-                    if(checkPurchased($userID)) {
-                        $response = "You're all set! <a href='signin'>Sign In here</a>!";
-                    } else {
-                        $response = "Thank you for signing up! The next step is to purchase the Buggy plan for your company. You can do that <a href='purchase'>here</a>!";
-                    }
-                } else { //developer account
-                    $response = "Thank you for verifying your email! <a href='signin'>Sign In here</a>!";
+            //check if management account needs purchase
+            $userID = getUserID($_GET['email']);
+            if (getAccountType($userID) == "management") { //management account
+                if (checkPurchased($userID)) {
+                    $response = "You're all set! <a href='signin'>Sign In here</a>!";
+                } else {
+                    $response = "Thank you for signing up! The next step is to purchase the Buggy plan for your company. You can do that <a href='purchase'>here</a>!";
                 }
-            } else {
-                $response = "You have already verified your email. <a href='signin'>Sign In here</a>!";
+            } else { //developer account
+                $response = "Thank you for verifying your email! <a href='signin'>Sign In here</a>!";
             }
         } else {
-            $response = "Broken link! Please click the link in your email.";
+            $response = "You have already verified your email. <a href='signin'>Sign In here</a>!";
         }
     } else {
         $response = "Broken link! Please click the link in your email.";
     }
+} else {
+    $response = "Broken link! Please click the link in your email.";
+}
+// vs code changes checking things out
 ?>
+
 <body>
     <?php printSidebar("notloggedin", ""); ?>
     <div class="main">
@@ -46,4 +48,5 @@
     <?php printFooter("basic"); ?>
 
 </body>
+
 </html>
