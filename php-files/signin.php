5b8b14143552d15126c44d390a5e9977c31953e2
Tyler J. Smith <smithtj921@gmail.com>
2021-03-03 10:25:07 -0500

    changes to sign in

--- php-files/signin.php
+++ php-files/signin.php
@@ -9,16 +9,16 @@ function checkLogin($email, $password) {
         $userID = getUserID($email);
         $companyID = getUserInfo($userID, "associatedCompany");
 
-        if((getAccountType($userID) == "management") && (getCompanyInfo($companyID, "purchased") == 0)) {
-            $result = "notPurchased";
-        } else {
+        // if((getAccountType($userID) == "management") && (getCompanyInfo($companyID, "purchased") == 0)) {
+        //     $result = "notPurchased";
+        // } else {
             $db = db_connect();
             $values = [$email, $password];
             $sql = "SELECT id, accountType FROM userinfo WHERE email = ? AND password = md5( ? )";
             $stmt = $db->prepare($sql);
             $stmt->execute($values);
             $result = $stmt->fetch(PDO::FETCH_ASSOC);
-        }
+        // }
         return $result;
     } catch (Exception $e) {
         return NULL;
@@ -28,4 +28,3 @@ function checkLogin($email, $password) {
 }
 
 $db = NULL;
-?>
