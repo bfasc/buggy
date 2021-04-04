a3486ee9e9106831c5f1fcada076761f2f8cb539
liz-lower <elizabethlower98@gmail.com>
2021-04-04 13:37:56 -0400

    Company Management Styling

--- companymanagement.php
+++ companymanagement.php
@@ -6,14 +6,42 @@ printHead("Manage Company | Buggy - Let's Code Together");
 <body>
     <?php printSidebar(getAccountType($_SESSION['userID']), "companymanagement"); ?>
 
-    <div class="main">
+    <div class="main" id="company">
         <?php printHeader($_SESSION['userID']);
         ?>
-        <h2 class='subhead'>Want to invite an employee to work with your company in Buggy projects?</h2 class='subhead'>
-        <p>Give your developer this link: <a>http://projectbuggy.tk/signup?companyCode=<?php echo getCompanyInfo(getUserInfo($_SESSION['userID'], "associatedCompany"), "companyCode"); ?></a> or this code: <a><?php echo getCompanyInfo(getUserInfo($_SESSION['userID'], "associatedCompany"), "companyCode"); ?></a> to fill in the signup form.</p>
+        <h2 class='subhead'>Company Management</h2 class='subhead'>
         <a href="companyreports" class="button">Generate Reports</a>
+
+        <div class='copylink-wrap'>
+            <h3>Want to invite an employee to work with your company in Buggy projects?</h3>
+            <p>Give your developer this link: </p>
+            <textarea disabled id='copylink'>http://projectbuggy.tk/signup?companyCode=<?php echo getCompanyInfo(getUserInfo($_SESSION['userID'], "associatedCompany"), "companyCode"); ?></textarea>
+            <a class='copy button' id='copylink'>Copy Link</a>
+            <p>Or this code to enter in the signup form: </p>
+            <textarea disabled id='code'><?php echo getCompanyInfo(getUserInfo($_SESSION['userID'], "associatedCompany"), "companyCode"); ?></textarea>
+            <a class='copy button' id='code'>Copy Code</a>
+        </div>
     </div>
 
+    <script>
+    function copy(id) {
+        var copyText = document.getElementById(id);
+        copyText.disabled = false;
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+        copyText.disabled = true;
+        window.getSelection().removeAllRanges();
+        if(id == 'copylink')
+            alert("Copied Link");
+        else
+            alert("Copied Code");
+    }
+    $('.copy').click(function(){
+        copy($(this).attr('id'));
+    });
+
+    </script>
     <?php printFooter("basic"); ?>
 </body>
 
