e2baa6b61eac106d8e92683329dc2dbc9c74f118
liz-lower <elizabethlower98@gmail.com>
2021-04-18 18:35:45 -0400

    Selected priority

--- search.php
+++ search.php
@@ -110,6 +110,8 @@ printHead("Search Tickets | Buggy - Let's Code Together");
         </section>
     </div>
 
+    <script src="scripts/forms.js"></script>
+    <script src="scripts/priority.js"></script>
     <script>
         //TODO: click priority makes empty
         $('#stars li').on('click', function(){
@@ -269,8 +271,6 @@ printHead("Search Tickets | Buggy - Let's Code Together");
             });
         });
     </script>
-    <script src="scripts/forms.js"></script>
-    <script src="scripts/priority.js"></script>
 
     <?php printFooter("basic"); ?>
 </body>
