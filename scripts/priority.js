4c84002539d13ffbf7b77ec3172942092b339838
liz-lower <elizabethlower98@gmail.com>
2021-04-18 18:55:04 -0400

    Selected priority

--- scripts/priority.js
+++ scripts/priority.js
@@ -44,6 +44,17 @@
               $(stars[i]).removeClass('selected');
             }
             unClick = false;
+        } else {
+            var onStar = parseInt($(this).data('value'), 10); // The star currently selected
+            var stars = $(this).parent().children('li.star');
+
+            for (i = 0; i < stars.length; i++) {
+              $(stars[i]).removeClass('selected');
+            }
+
+            for (i = 0; i < onStar; i++) {
+              $(stars[i]).addClass('selected');
+            }
         }
     }
     console.log(unClick);
